17b3e69d591a69f59e7265e0cce28e2c886978cf
Myoty <160827496+Myoty@users.noreply.github.com>
2024-11-26 14:58:59 +1100

    Update toggle-mode.js

--- Javascript/toggle-mode.js
+++ Javascript/toggle-mode.js
@@ -1,11 +1,9 @@
 document.querySelector('[aria-label="Toggle Light Mode"]').addEventListener('click', () => {
-    document.body.classList.add('light-mode');
-    document.body.classList.remove('dark-mode');
+    document.body.classList.toggle('light-mode');
 });
 
 document.querySelector('[aria-label="Toggle Dark Mode"]').addEventListener('click', () => {
-    document.body.classList.add('dark-mode');
-    document.body.classList.remove('light-mode');
+    document.body.classList.toggle('dark-mode');
 });
 
 document.querySelector('[data-accessibility="contrast"]').addEventListener('click', () => {
