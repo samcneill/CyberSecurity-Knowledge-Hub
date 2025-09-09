1a3270ec00606da070f4cfdee9d6c87df3434b11
samcneill <siranimcneill020@gmail.com>
2025-09-09 16:52:20 +1000

    testing.html, testing.js, style.css changed

---
+++ Javascript/cybersecurity/testing.js
@@ -0,0 +1,7 @@
+document.getElementById("pageBgPicker").addEventListener("input", function () {
+  document.body.style.backgroundColor = this.value;
+});
+
+document.getElementById("pageTextPicker").addEventListener("input", function () {
+  document.body.style.color = this.value;
+});
